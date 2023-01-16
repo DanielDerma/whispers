97772428eb5b95f80e08893a7b49ce7a3eca1de5
DanielDerma <danielderma94@gmail.com>
2023-01-16 15:19:43 -0600

    add button add group

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,22 @@
 import Contact from "./Contact";
+import Plus from "@/utils/icons/Plus";
 
 const App = () => {
   return (
-    <div className="h-full overflow-hidden pt-6">
-      <h1 className="mb-4 px-9">Chatting Room</h1>
-      <section className="flex h-full flex-col gap-y-6 overflow-y-auto px-10 pb-16">
+    <div className="relative h-full overflow-hidden pt-6">
+      <h1 className="mx-9 mb-4">Chatting Room</h1>
+      <section className="flex h-full flex-col gap-y-6 overflow-y-auto px-10 pb-28">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15].map((_, i) => (
           <Contact key={i} lastMessage="Hello, how are you?" name="Cesar" />
         ))}
       </section>
+      <div className="absolute bottom-0 z-50 flex h-8 w-full justify-center border-t-2 border-black bg-white">
+        <div className="relative bottom-6 grid h-10 w-10 place-content-center rounded-full bg-[#ff5858] p-6">
+          <div className="grid place-content-center rounded-full bg-white">
+            <Plus className="h-6 w-6 text-[#ff5858]" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
