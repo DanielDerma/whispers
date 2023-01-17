48d9767e2b59212e12768d2c3073501aa08de172
DanielDerma <danielderma94@gmail.com>
2023-01-16 18:59:00 -0600

    change text modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -72,7 +72,7 @@ export default function MyModal() {
                     as="h3"
                     className="text-lg leading-6 text-gray-900"
                   >
-                    {isAdd ? "Room created" : "Create a room"}
+                    {isAdd ? "Room created 🎉🎉🎉" : "Create a room"}
                   </Dialog.Title>
                   {isAdd ? (
                     <>
@@ -94,7 +94,7 @@ export default function MyModal() {
                         </div>
                       </div>
 
-                      <div className="mt-4">
+                      <div className="mt-4 text-right">
                         <button
                           type="button"
                           className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
